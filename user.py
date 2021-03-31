e0d5f89ee63f9fd1e5efa79c798c96dfe9fbff8c
Aranda Paige <ArandaPaige@gmail.com>
2021-03-31 13:16:49 -0500

    Added more sqlite functions.

--- user.py
+++ user.py
@@ -1,5 +1,7 @@
 import sqlite3
+import datetime
 
+DATETODAY = datetime.date.today()
 DATABASE = 'user.db'
 
 
@@ -10,8 +12,9 @@ class User:
     their weight loss/gain.
     '''
 
-    def __init__(self, username=None, name=None, starting_weight=None, current_weight=None, height=None,
+    def __init__(self, name=None, starting_weight=None, current_weight=None, height=None,
                  weight_history=None):
+        self.ID = 1
         self.name = name
         self.starting_weight = starting_weight
         self.current_weight = current_weight
@@ -27,20 +30,42 @@ class User:
     def create_user_table(self):
         db = sqlite3.connect(DATABASE)
         cur = db.cursor()
-        cur.execute('''CREATE TABLE USER
-                (ID INT PRIMARY KEY NOT NULL,
-                NAME            TEXT NOT NULL,
-                WEIGHT          REAL NOT NULL,
-                HEIGHT          INT NOT NULL);'''
+        cur.execute('''
+        CREATE TABLE USER
+            (ID INT PRIMARY KEY NOT NULL,
+            NAME            TEXT NOT NULL,
+            WEIGHT          REAL NOT NULL,
+            HEIGHT          INT NOT NULL);'''
         )
+        db.commit()
+        db.close()
+
+    def insert_user(self):
+        db = sqlite3.connect(DATABASE)
+        cur = db.cursor()
+        cur.execute('''
+        INSERT INTO USER (ID,NAME,WEIGHT,HEIGHT) \
+            VALUES (?,?,?,?)'''), (self.ID, self.name, self.current_weight, self.height)
+        db.commit()
+        db.close()
+
 
     def create_weight_history_table(self):
         db = sqlite3.connect(DATABASE)
         cur = db.cursor()
-        cur.execute('''CREATE TABLE WEIGHT_HISTORY
-                (DATE TEXT PRIMARY KEY NOT NULL,
-                WEIGHT             REAL NOT NULL);'''
+        cur.execute('''
+        CREATE TABLE WEIGHT_HISTORY
+            (DATE TEXT PRIMARY KEY NOT NULL,
+            WEIGHT             REAL NOT NULL);'''
         )
+        db.commit()
+        db.close()
+
+    def insert_weight_entry(self):
+        pass
+
+    def delete_weight_entry(self):
+        pass
 
     def user_setup(self, name=None, starting_weight=None, current_weight=None, height=None):
         if name != None:
@@ -67,3 +92,32 @@ class User:
 
     def set_height(self):
         pass
+
+    def user_dict_create(self, name, startingweight, currentweight, height, weight_history=None):
+        '''
+        Creates a dictionary with all of the user's personal statistics to be serialized as JSON.
+        :param name: User's full name
+        :param startingweight: The user's starting weight.
+        :param currentweight: The user's current weight.
+        :param height: The user's height.
+        :param weight_history: User's weight history is mapped by date.
+        :return Dictionary: a dictionary containing the user's personal statistics.
+        '''
+        if weight_history == None:
+            return {
+                'name': name,
+                'starting weight': startingweight,
+                'current weight': currentweight,
+                'height': height,
+                'weight history': {
+                    str(DATETODAY): currentweight
+                }
+            }
+        else:
+            return {
+                'name': name,
+                'starting weight': startingweight,
+                'current weight': currentweight,
+                'height': height,
+                'weight history': weight_history
+            }
