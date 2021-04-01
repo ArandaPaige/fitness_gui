f6b928d2b6ac59b3b05c4eae7501a2582b644f48
Aranda Paige <ArandaPaige@gmail.com>
2021-03-31 23:07:17 -0500

    Refactored the database and turned into module

--- database.py
+++ database.py
@@ -6,82 +6,81 @@ DATABASE = 'user.db'
 DATABASE_PATH = BASE_DIR / DATABASE
 
 
-class UserDB(sqlite3):
-    '''Class docstring'''
-
-    def __init__(self):
-        super().__init__()
-        self.user = None
-
-    @classmethod
-    def create_database(cls):
-        if DATABASE_PATH.exists() == False:
-            pass
+def create_database():
+    if DATABASE_PATH.exists() == False:
+        create_user_table()
+        create_weight_history_table()
+    else:
+        retrieve_user(user_id=1)
 
 
-    @classmethod
-    def create_user_table(cls):
-        db = sqlite3.connect(DATABASE)
-        cur = db.cursor()
-        cur.execute('''
-        CREATE TABLE USER
-            (ID INT PRIMARY KEY NOT NULL,
-            NAME            TEXT NOT NULL,
-            WEIGHT          REAL NOT NULL,
-            HEIGHT          INT NOT NULL);'''
-                    )
-        db.commit()
-        db.close()
+def create_user_table():
+    db = sqlite3.connect(DATABASE)
+    cur = db.cursor()
+    cur.execute('''
+    CREATE TABLE USER
+        (ID INT PRIMARY KEY NOT NULL,
+        NAME            TEXT NOT NULL,
+        WEIGHT          REAL NOT NULL,
+        HEIGHT          INT NOT NULL);'''
+                )
+    db.commit()
+    db.close()
 
-    @classmethod
-    def create_weight_history_table(cls):
-        db = sqlite3.connect(DATABASE)
-        cur = db.cursor()
-        cur.execute('''
-        CREATE TABLE WEIGHT_HISTORY
-            (DATE TEXT PRIMARY KEY NOT NULL,
-            WEIGHT             REAL NOT NULL);'''
-                    )
-        db.commit()
-        db.close()
 
-    def retrieve_user(self, user_id):
-        db = sqlite3.connect(DATABASE)
-        cur = db.cursor()
-        if cur.execute("SELECT ID from USER where ID = 1") == "":
-            pass
-        else:
-            cur.execute("SELECT ID, NAME, WEIGHT, HEIGHT from USER where ID = 1")
+def create_weight_history_table():
+    db = sqlite3.connect(DATABASE)
+    cur = db.cursor()
+    cur.execute('''
+    CREATE TABLE WEIGHT_HISTORY
+        (DATE TEXT PRIMARY KEY NOT NULL,
+        WEIGHT             REAL NOT NULL);'''
+                )
+    db.commit()
+    db.close()
 
-    def insert_user(self, user_id, name, weight, height):
-        db = sqlite3.connect(DATABASE)
-        cur = db.cursor()
-        cur.execute('''
-        INSERT INTO USER (ID,NAME,WEIGHT,HEIGHT) \
-            VALUES (?,?,?,?)'''), (user_id, name, weight, height)
-        db.commit()
-        db.close()
 
-    def insert_weight_entry(self, date, weight):
-        db = sqlite3.connect(DATABASE)
-        cur = db.cursor()
-        cur.execute('''
-        INSERT INTO WEIGHT_HISTORY (DATE, WEIGHT) \
-            VALUES (?,?)'''), (date, weight)
-        db.commit()
-        db.close()
+def retrieve_user(user_id):
+    db = sqlite3.connect(DATABASE)
+    cur = db.cursor()
+    if cur.execute("SELECT ID from USER where ID = ?", (user_id,)) == "":
+        pass
+    else:
+        cur.execute("SELECT ID, NAME, WEIGHT, HEIGHT from USER where ID = ?", (user_id,))
 
-    def update_weight_entry(self, date, weight):
-        db = sqlite3.connect(DATABASE)
-        cur = db.cursor()
-        cur.execute('''
-        UPDATE WEIGHT_HISTORY SET WEIGHT = ? WHERE ID = ?'''), (weight, date)
-        db.commit()
-        db.close()
 
-    def delete_weight_entry(self, date):
-        db = sqlite3.connect(DATABASE)
-        cur = db.cursor()
-        cur.execute('''DELETE from WEIGHT_HISTORY where DATE = ?'''), (date,)
-        db.commit()
-        db.close()
+def insert_user(user):
+    db = sqlite3.connect(DATABASE)
+    cur = db.cursor()
+    cur.execute('''
+    INSERT INTO USER (ID,NAME,WEIGHT,HEIGHT) \
+        VALUES (?,?,?,?)'''), (user.user_id, user.name, user.weight, user.height)
+    db.commit()
+    db.close()
+
+
+def insert_weight_entry(date, weight):
+    db = sqlite3.connect(DATABASE)
+    cur = db.cursor()
+    cur.execute('''
+    INSERT INTO WEIGHT_HISTORY (DATE, WEIGHT) \
+        VALUES (?,?)'''), (date, weight)
+    db.commit()
+    db.close()
+
+
+def update_weight_entry(date, weight):
+    db = sqlite3.connect(DATABASE)
+    cur = db.cursor()
+    cur.execute('''
+    UPDATE WEIGHT_HISTORY SET WEIGHT = ? WHERE ID = ?'''), (weight, date)
+    db.commit()
+    db.close()
+
+
+def delete_weight_entry(date):
+    db = sqlite3.connect(DATABASE)
+    cur = db.cursor()
+    cur.execute('''DELETE from WEIGHT_HISTORY where DATE = ?'''), (date,)
+    db.commit()
+    db.close()
--- user.py
+++ user.py
@@ -1,4 +1,5 @@
 import datetime
+import database
 
 DATETODAY = datetime.date.today()
 
